bc8b46bf1d44f947cfde0513b951d5bc366a24a3
yillkid <yillkid@gmail.com>
2024-08-15 22:13:51 +0800

    number_append

--- calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -9,10 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private TextView display;
-    private double firstValue = 0;
-    private double secondValue = 0;
-    private String operator = "";
-    private boolean operatorSelected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,52 +22,10 @@ public class MainActivity extends AppCompatActivity {
     public void onNumberClick(View view) {
         Button button = (Button) view;
         String value = button.getText().toString();
-
-        if (operatorSelected) {
+        if (display.getText().toString().equals("0")) {
             display.setText(value);
-            operatorSelected = false;
         } else {
-            if (display.getText().toString().equals("0")) {
-                display.setText(value);
-            } else {
-                display.append(value);
-            }
+            display.append(value);
         }
-    }
-
-    // 處理操作符按鍵點擊事件
-    public void onOperatorClick(View view) {
-        Button button = (Button) view;
-        operator = button.getText().toString();
-        firstValue = Double.parseDouble(display.getText().toString());
-        operatorSelected = true;
-    }
-
-    // 處理等於按鍵點擊事件
-    public void onEqualClick(View view) {
-        secondValue = Double.parseDouble(display.getText().toString());
-        double result = 0;
-
-        switch (operator) {
-            case "+":
-                result = firstValue + secondValue;
-                break;
-            case "-":
-                result = firstValue - secondValue;
-                break;
-        }
-
-        display.setText(String.valueOf(result));
-        operator = "";
-        operatorSelected = true;
-    }
-
-    // 處理清除按鍵點擊事件
-    public void onClearClick(View view) {
-        display.setText("0");
-        firstValue = 0;
-        secondValue = 0;
-        operator = "";
-        operatorSelected = false;
     }
 }
